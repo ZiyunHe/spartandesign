a9b41d88bfe0dc178c9dcd2eb04e1322730fe357
Jeremiah Grande <jeregrande@yahoo.com>
2017-11-29 20:49:37 -0800

    Add files via upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
 	root to: 'posts#index'
-  resources :posts
+  resources :posts do 
+  	resources :comments do
+  		member do
+  			put "like" => "comments#upvote"
+  			put "unlike" => "comments#downvote"
+  		end
+  	end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
